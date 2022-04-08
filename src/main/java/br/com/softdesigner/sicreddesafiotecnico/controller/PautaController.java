b2cfcc162735499d3ea52d19f6472979a34ffa49
Gedalias Caldas <ctl_gcaldas@uolinc.com>
2022-04-08 06:23:24 -0300

    feat: Criado método de busca de pauta por id

--- src/main/java/br/com/softdesigner/sicreddesafiotecnico/controller/PautaController.java
+++ src/main/java/br/com/softdesigner/sicreddesafiotecnico/controller/PautaController.java
@@ -4,10 +4,7 @@ import br.com.softdesigner.sicreddesafiotecnico.dto.CreatePautaDTO;
 import br.com.softdesigner.sicreddesafiotecnico.dto.PautaDTO;
 import br.com.softdesigner.sicreddesafiotecnico.service.PautaService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -28,8 +25,15 @@ public class PautaController {
     }
 
     @PostMapping
-    public ResponseEntity createPauta(CreatePautaDTO createPautaDTO) {
+    public ResponseEntity<Mono<PautaDTO>> createPauta(CreatePautaDTO createPautaDTO) {
         Mono<PautaDTO> createdPauta = pautaService.createPauta(createPautaDTO);
         return ResponseEntity.status(CREATED).body(createdPauta);
+    }
+
+    @GetMapping("/{id}")
+    public Mono<ResponseEntity<PautaDTO>> findById(@PathVariable String id) {
+        return pautaService.findById(id)
+            .map(pautaDTO -> ResponseEntity.ok(pautaDTO))
+            .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 }
