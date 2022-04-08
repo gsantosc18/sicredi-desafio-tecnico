3b2db7fe609863451778d59d53298e494af0333f
Gedalias Caldas <ctl_gcaldas@uolinc.com>
2022-04-08 06:24:25 -0300

    fix: Adicionada anotação do spring nos construtores IOC

--- src/main/java/br/com/softdesigner/sicreddesafiotecnico/repository/PautaRepository.java
+++ src/main/java/br/com/softdesigner/sicreddesafiotecnico/repository/PautaRepository.java
@@ -2,6 +2,8 @@ package br.com.softdesigner.sicreddesafiotecnico.repository;
 
 import br.com.softdesigner.sicreddesafiotecnico.document.PautaDocument;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PautaRepository extends ReactiveCrudRepository<PautaDocument, String> {
 }
--- src/main/java/br/com/softdesigner/sicreddesafiotecnico/service/PautaService.java
+++ src/main/java/br/com/softdesigner/sicreddesafiotecnico/service/PautaService.java
@@ -5,6 +5,7 @@ import br.com.softdesigner.sicreddesafiotecnico.document.PautaDocument;
 import br.com.softdesigner.sicreddesafiotecnico.dto.CreatePautaDTO;
 import br.com.softdesigner.sicreddesafiotecnico.dto.PautaDTO;
 import br.com.softdesigner.sicreddesafiotecnico.repository.PautaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -15,6 +16,7 @@ import java.util.UUID;
 public class PautaService {
     private final PautaRepository pautaRepository;
 
+    @Autowired
     public PautaService(PautaRepository pautaRepository) {
         this.pautaRepository = pautaRepository;
     }
