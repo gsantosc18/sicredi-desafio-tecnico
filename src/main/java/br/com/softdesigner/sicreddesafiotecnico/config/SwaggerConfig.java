9202e38bc208dc53980041e46d387ae5675c29e1
Gedalias Caldas <ctl_gcaldas@uolinc.com>
2022-04-09 01:28:20 -0300

    chore(Gradle): Adicionado a dependencia do spring open api

---
+++ src/main/java/br/com/softdesigner/sicreddesafiotecnico/config/SwaggerConfig.java
@@ -0,0 +1,14 @@
+package br.com.softdesigner.sicreddesafiotecnico.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(
+        title = "Desafio técnico - Sicredi",
+        description = "Vaga Backend",
+        version = "1.0"
+))
+public class SwaggerConfig {
+}
