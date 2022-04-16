3cc6a75d338d4a09f28fba01f5ba863df1692dcf
Gedalias Caldas <ctl_gcaldas@uolinc.com>
2022-04-16 19:35:19 -0300

    fix: Correção de pesquisa de voto pelo id da sessão

--- src/main/java/br/com/softdesigner/sicreddesafiotecnico/repository/VotoRepository.java
+++ src/main/java/br/com/softdesigner/sicreddesafiotecnico/repository/VotoRepository.java
@@ -3,6 +3,7 @@ package br.com.softdesigner.sicreddesafiotecnico.repository;
 import br.com.softdesigner.sicreddesafiotecnico.document.AssociadoDocument;
 import br.com.softdesigner.sicreddesafiotecnico.document.SessaoDocument;
 import br.com.softdesigner.sicreddesafiotecnico.document.VotoDocument;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
@@ -11,5 +12,6 @@ import reactor.core.publisher.Mono;
 @Repository
 public interface VotoRepository extends ReactiveCrudRepository<VotoDocument, String> {
     Mono<VotoDocument> findByAssociadoAndSessao(AssociadoDocument associadoDocument, SessaoDocument sessaoDocument);
+    @Query(value = "{'sessao.id': ?0 }")
     Flux<VotoDocument> findBySessaoId(String sessaoId);
 }
